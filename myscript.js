9f8534f2ecac4af5557841b31bce7e0f88f1babd
davidnegus <david_2_9@hotmail.co.uk>
2017-04-30 18:12:52 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -17,3 +17,23 @@ function showSlides() {
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 7000); // Change image every 2 seconds
 };
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
